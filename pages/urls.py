8a36a71b2abd533cf134238262df46b6c8679894
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-03-17 19:12:56 -0300

    fix typos in urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = {
+urlpatterns = [
     path('', IndexView.as_view(), name='inicio'),
     path('catalogo', CatalogoView.as_view(), name='catalogo'),
     path('sobre', SobreView.as_view(), name='sobre'),
     
-}
+]
