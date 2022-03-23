a19b456496cbfa2dc51bfe9a96cac04e6e880bf2
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-03-23 01:38:30 -0300

    Refactor Django project name 'Minimercado' -> 'minimercado'

--- minimercado/asgi.py
+++ minimercado/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Minimercado project.
+ASGI config for minimercado project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Minimercado.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minimercado.settings')
 
 application = get_asgi_application()
--- minimercado/wsgi.py
+++ minimercado/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Minimercado project.
+WSGI config for minimercado project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Minimercado.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minimercado.settings')
 
 application = get_wsgi_application()
