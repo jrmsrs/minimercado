63f3e9f1afee451d39b8cd1632b046bac42c3312
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-03-23 01:30:27 -0300

    add first models

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    name = models.CharField(max_length=50,unique=True,verbose_name="Nome")
+    price = models.DecimalField(max_digits=8, decimal_places=2,verbose_name="Preço")
+
+    class Meta:
+        verbose_name = "Produto"
+
+    def __str__(self):
+        return "{} (R${})".format(self.name,self.price)
+    
+
+class Sale(models.Model):
+
+    qty = models.IntegerField(verbose_name="Quantidade")
+    date = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name="Data")
+    product_name = models.ForeignKey(Product, verbose_name="Produto", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Venda"
+
+    def __str__(self):
+        return f"{self.product_name} {self.qty}x @ {self.date.strftime('%d-%m-%Y | %H:%M')}"
