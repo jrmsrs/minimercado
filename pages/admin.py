d508d05174c00fe8bb4273229e541c09c8877ba2
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-03-23 01:30:27 -0300

    add first models

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# app_name.models
+from pages.models import Product, Sale
+
+# Register your models to admin site, then you can add, edit, delete and search your models in Django admin site.
+admin.site.register(Product)
+admin.site.register(Sale)
