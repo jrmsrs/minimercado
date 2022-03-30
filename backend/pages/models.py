c142e54c15da4ae6e8ddcafd1ad603e189f892ca
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-03-30 01:35:43 -0300

    add sector, category models

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -1,7 +1,26 @@
 from django.db import models
 
-class Product(models.Model):
+class Sector(models.Model):
+    name = models.CharField(max_length=50,unique=True,verbose_name="Nome")
+    
+    class Meta:
+        verbose_name = "Setor"
 
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    setor = models.ForeignKey(Sector, verbose_name="Setor", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50,unique=True,verbose_name="Nome")
+
+    class Meta:
+        verbose_name = "Categoria"
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, verbose_name="Categoria", on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=50,unique=True,verbose_name="Nome")
     price = models.DecimalField(max_digits=8, decimal_places=2,verbose_name="Preço")
 
@@ -13,13 +32,12 @@ class Product(models.Model):
     
 
 class Sale(models.Model):
-
+    product = models.ForeignKey(Product, verbose_name="Produto", on_delete=models.CASCADE)
     qty = models.IntegerField(verbose_name="Quantidade")
     date = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name="Data")
-    product_name = models.ForeignKey(Product, verbose_name="Produto", on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "Venda"
 
     def __str__(self):
-        return f"{self.product_name} {self.qty}x @ {self.date.strftime('%d-%m-%Y | %H:%M')}"
+        return f"{self.product} {self.qty}x @ {self.date.strftime('%d-%m-%Y | %H:%M')}"
