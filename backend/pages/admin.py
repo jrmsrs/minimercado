98496bd4efbe613928ebbeec48727fe931bdf5a2
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-03-30 01:35:43 -0300

    [backend] add sector, category models

--- backend/pages/admin.py
+++ backend/pages/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 # Register your models here.
 
 # app_name.models
-from pages.models import Product, Sale
+from pages.models import Sector, Category, Product, Sale
 
 # Register your models to admin site, then you can add, edit, delete and search your models in Django admin site.
+
+admin.site.register(Sector)
+admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Sale)
