3f16ab886e80b200fe6bd9ddd8b4898a5996b27a
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-03-31 02:47:04 -0300

    setup for React template; include cors dependecies

--- backend/minimercado/urls.py
+++ backend/minimercado/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('administrativum/', admin.site.urls),
-    path('', include('pages.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    # path('', include('pages.urls')),
     path('api/', include('api.urls')),
+]
+
+urlpatterns += [
+    re_path(r'^.*', TemplateView.as_view(template_name="index.html"))
 ]
