4a6a49e99b4e32b3d8018410c15e192a430c56ef
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-04-05 04:08:27 -0300

    add first pages, routes and main layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className="container text-light">
+        <Routes>
+          <Route 
+            exact path="/"
+            element={<Home />}
+          />
+          <Route 
+            path="/sobre"
+            element={<About />}
+          />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
