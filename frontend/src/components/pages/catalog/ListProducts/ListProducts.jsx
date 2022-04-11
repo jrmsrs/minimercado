6c763b5a591b0741027a34caac7888a65076d531
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-04-11 19:22:41 -0300

    [frontend] fix React class/className warnings

--- frontend/src/components/pages/catalog/ListProducts/ListProducts.jsx
+++ frontend/src/components/pages/catalog/ListProducts/ListProducts.jsx
@@ -12,33 +12,33 @@ function ListProducts() {
   
     return (
         <>
-        <div style={{fontSize:"0.5rem"}}>&nbsp;</div>
-        <div className='row'>
-        {products.map( product => 
-            <div key={product.id} className="col-6 col-sm-4 col-md-3 col-xl-2 mb-4">
-                <div className="card mb-sm-2 h-100">
-                    <img src="https://avatars.githubusercontent.com/u/38083522?v=4" className="card-img-top" alt="..." />
-                    <div class="card-body text-dark">
-                        <h6 class="card-title">
-                            {product.name}
-                        </h6>
-                        <p class="card-text font-weight-normal">
-                            {product.price}
-                        </p>
-                        
-                    </div>
-                    <div className="card-header">
-                        <a 
-                          href={'https://wa.me/?text=Olá! Eu tenho interesse em ' + product.name.toUpperCase()} 
-                          className="btn btn-primary btn-sm" 
-                          target="_blank" rel="noreferrer">
-                            Comprar
-                        </a>
+            <div style={{fontSize:"0.5rem"}}>&nbsp;</div>
+            <div className='row'>
+            {products.map( product => 
+                <div key={product.id} className="col-6 col-sm-4 col-md-3 col-xl-2 mb-4">
+                    <div className="card mb-sm-2 h-100">
+                        <img src="https://avatars.githubusercontent.com/u/38083522?v=4" className="card-img-top" alt="..." />
+                        <div className="card-body text-dark">
+                            <h6 className="card-title">
+                                {product.name}
+                            </h6>
+                            <p className="card-text font-weight-normal">
+                                {product.price}
+                            </p>
+                            
+                        </div>
+                        <div className="card-header">
+                            <a 
+                            href={'https://wa.me/?text=Olá! Eu tenho interesse em ' + product.name.toUpperCase()} 
+                            className="btn btn-primary btn-sm" 
+                            target="_blank" rel="noreferrer">
+                                Comprar
+                            </a>
+                        </div>
                     </div>
                 </div>
+            )}
             </div>
-        )}
-        </div>
         </>
     )
   }
