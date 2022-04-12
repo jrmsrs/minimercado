fe8e78cd3abbc6e6a5b58b61016873700362322f
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-04-12 02:07:14 -0300

    [frontend] update html metas; update navbar

--- frontend/src/components/layout/Header/Header.jsx
+++ frontend/src/components/layout/Header/Header.jsx
@@ -1,7 +1,14 @@
 import { Link } from 'react-router-dom'
+import './Header.css'
 
 function Header() {
+    function handleCollapse() {
+      document.getElementById("navbarCollapse").classList.remove("show")
+      document.getElementById("navbarBtn").classList.add("collapsed")
+    }
+
     return (
+      
         <div className="mb-5">
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
           <div className="container">
@@ -11,25 +18,25 @@ function Header() {
             </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
             data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" 
-            aria-expanded="false" aria-label="Toggle navigation">
+            aria-expanded="false" aria-label="Toggle navigation" id="navbarBtn">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarCollapse">
               <ul className="navbar-nav me-auto mb-2 mb-md-0">
                 <li className="nav-item">
-                  <Link className="nav-link" to="/">Inicio</Link>
+                  <Link className="nav-link" to="/" onClick={handleCollapse}>Inicio</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/catalogo">Catalogo</Link>
+                  <Link className="nav-link" to="/catalogo" onClick={handleCollapse}>Catalogo</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/sobre">Sobre</Link>
+                  <Link className="nav-link" to="/sobre" onClick={handleCollapse}>Sobre</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link disabled" to="/x">Área <span className="d-sm-inline d-md-none d-lg-inline">restrita</span> </Link>
+                  <Link className="nav-link disabled" to="/x" onClick={handleCollapse}>Área <span className="d-sm-inline d-md-none d-lg-inline">restrita</span> </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/add">add</Link>
+                  <Link className="nav-link" to="/add" onClick={handleCollapse}>add</Link>
                 </li>
               </ul>
             </div>
