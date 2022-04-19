2b7887244f3b68a89dc363df1bacdcec25333060
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-04-19 16:49:42 -0300

    [backend] allow drf single instance get/put/delete

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import SectorView, CategoryView, ProductView, SaleView
+from .views import SectorView, SectorSingleView, CategoryView, CategorySingleView, ProductView, ProductSingleView, SaleView, SaleSingleView
 
 urlpatterns = [
     path('sector', SectorView.as_view()),
+    path('sector/<pk>', SectorSingleView.as_view()),
     path('category', CategoryView.as_view()),
+    path('category/<pk>', CategorySingleView.as_view()),
     path('product', ProductView.as_view()),
+    path('product/<pk>', ProductSingleView.as_view()),
     path('sale', SaleView.as_view()),
+    path('sale/<pk>', SaleSingleView.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,15 @@ class SectorView(generics.ListCreateAPIView):
     queryset = Sector.objects.all()
     serializer_class = SectorSerializer
 
+class SectorSingleView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Sector.objects.all()
+    serializer_class = SectorSerializer
+
 class CategoryView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategorySingleView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
@@ -16,7 +24,14 @@ class ProductView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+class ProductSingleView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
 class SaleView(generics.ListCreateAPIView):
     queryset = Sale.objects.all()
     serializer_class = SaleSerializer
 
+class SaleSingleView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer
